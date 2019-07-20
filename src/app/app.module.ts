bcb0a61cd71d9106cf1a3070946b4dd25ee0ae92
Daniel Foote <danfoote104227@gmail.com>
2019-07-20 13:51:16 -0400

    Home page initial commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AppHeaderComponent } from './header/app-header.component';
+import { HomeComponent } from './home/home.component';
+import { AppFooterComponent } from './footer/app-footer.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppHeaderComponent
+    AppHeaderComponent,
+    AppFooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
