5fa753f16693cf6fdbea041c8a2cc216b14e84d4
Daniel Foote <danfoote104227@gmail.com>
2019-07-20 16:38:50 -0400

    Added all components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,15 @@ import { AppComponent } from './app.component';
 import { AppHeaderComponent } from './header/app-header.component';
 import { HomeComponent } from './home/home.component';
 import { AppFooterComponent } from './footer/app-footer.component';
+import { AboutComponent } from './about/about.component';
+import { PhotoGalleryComponent } from './photo-gallery/photo-gallery.component';
+import { GuestPortalComponent } from './guest-portal/guest-portal.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'photo-gallery', component: PhotoGalleryComponent },
+  { path: 'guest-portal', component: GuestPortalComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
@@ -18,6 +24,9 @@ const appRoutes: Routes = [
     AppComponent,
     AppHeaderComponent,
     AppFooterComponent,
+    AboutComponent,
+    PhotoGalleryComponent,
+    GuestPortalComponent,
     HomeComponent
   ],
   imports: [
---
+++ src/app/guest-portal/guest-portal.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './guest-portal.component.html',
+  styleUrls: ['./guest-portal.component.css']
+})
+export class GuestPortalComponent {
+}
