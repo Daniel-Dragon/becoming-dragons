002387f6201a7bbc354d9e7dab2347087f8bfd07
Daniel Foote <danfoote104227@gmail.com>
2019-10-16 17:01:30 -0400

    Removed unused component.

--- src/app/guest-modal/guest-modal.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  templateUrl: './guest-modal.component.html',
-  styleUrls: ['./guest-modal.component.css']
-})
-export class GuestModalComponent {
-}
