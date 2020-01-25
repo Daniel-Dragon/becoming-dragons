270e37844de81c578941801ed320c5e325be2cd5
Daniel Foote <danfoote104227@gmail.com>
2020-01-25 15:33:42 -0500

    Added scroll to top button.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, isDevMode } from '@angular/core';
+import { Component, isDevMode, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +7,25 @@ import { Component, isDevMode } from '@angular/core';
 })
 export class AppComponent {
   isDevMode: boolean = isDevMode();
+  showScrollToTop = false;
   title = 'app';
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    // In chrome and some browser scroll is given to body tag
+    const pos = (document.documentElement.scrollTop || document.body.scrollTop) + document.documentElement.offsetHeight;
+    const max = document.documentElement.scrollHeight;
+    // pos/max will give you the distance between scroll bottom and and bottom of screen in percentage.
+    const percent = pos / max - 1;
+    if (percent > .10)   {
+      // Do your action here
+      this.showScrollToTop = true;
+    } else {
+      this.showScrollToTop = false;
+    }
+  }
+
+  public scrollToTop() {
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
+  }
 }
